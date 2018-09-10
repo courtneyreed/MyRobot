603bc02b7c90c7f3853ff5190d2ed5993495bf7b
Courtney Reed <creedc2002@gmail.com>
2018-09-10 11:27:29 -0400

    Auto stash before merge of "master" and "origin/master"

--- Robocode/src/crrb/Test.java
+++ Robocode/src/crrb/Test.java
@@ -36,9 +36,9 @@ public class Test extends AdvancedRobot {
     public void run() {
         while (true)
             ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            setTurnGunRightRadians(Math.PI);
+            setTurnRadarLeftRadians(Math.PI);
+            execute();
         }
     }
 
