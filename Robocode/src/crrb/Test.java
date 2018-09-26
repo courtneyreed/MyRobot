1cf4af0b5ce8f3bf10a759c69b01b5f30ba81279
Unknown <creedc2002@gmail.com>
2018-09-26 11:27:35 -0400

    added movement 9/26/18

--- Robocode/src/crrb/Test.java
+++ Robocode/src/crrb/Test.java
@@ -6,6 +6,7 @@ import robocode.Robot;
 import robocode.util.Utils;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.geom.*;
 
 /**
  * Created by eahscs on 9/12/2018.
@@ -13,6 +14,14 @@ import java.util.List;
 public class Test extends AdvancedRobot {
 
     List<WaveBullet> waves = new ArrayList<WaveBullet>();
+    static final double MAX_VELOCITY = 8;
+    static final double WALL_MARGIN = 25;
+    Point2D robotLocation;
+    Point2D enemyLocation;
+    double enemyDistance;
+    double enemyAbsoluteBearing;
+    double movementLateralAngle = 0.2;
+
 
     static int[] stats = new int[31]; // 31 is the number of unique GuessFactors we're using
     // Note: this must be odd number so we can get
@@ -114,7 +123,63 @@ public class Test extends AdvancedRobot {
         if (setFireBullet(power) != null)
             waves.add(newWave);
 
+        robotLocation = new Point2D.Double(getX(), getY());
+        enemyAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
+        enemyDistance = e.getDistance();
+        enemyLocation = vectorToLocation(enemyAbsoluteBearing, enemyDistance, robotLocation);
 
+        move();
+
+        setTurnRadarRightRadians(Utils.normalRelativeAngle(enemyAbsoluteBearing - getRadarHeadingRadians()) * 2);
+    }
+
+    void move() {
+        considerChangingDirection();
+        Point2D robotDestination = null;
+        double tries = 0;
+        do {
+            robotDestination = vectorToLocation(absoluteBearing(enemyLocation, robotLocation) + movementLateralAngle,
+                    enemyDistance * (1.1 - tries / 100.0), enemyLocation);
+            tries++;
+        } while (tries < 100 && !fieldRectangle(WALL_MARGIN).contains(robotDestination));
+        goTo(robotDestination);
+    }
+
+    void considerChangingDirection() {
+        // Change lateral direction at random
+        // Tweak this to go for flat movement
+        double flattenerFactor = 0.05;
+        if (Math.random() < flattenerFactor) {
+            movementLateralAngle *= -1;
+        }
+    }
+
+    RoundRectangle2D fieldRectangle(double margin) {
+        return new RoundRectangle2D.Double(margin, margin,
+                getBattleFieldWidth() - margin * 2, getBattleFieldHeight() - margin * 2, 75, 75);
+    }
+
+    void goTo(Point2D destination) {
+        double angle = Utils.normalRelativeAngle(absoluteBearing(robotLocation, destination) - getHeadingRadians());
+        double turnAngle = Math.atan(Math.tan(angle));
+        setTurnRightRadians(turnAngle);
+        setAhead(robotLocation.distance(destination) * (angle == turnAngle ? 1 : -1));
+        // Hit the brake pedal hard if we need to turn sharply
+        setMaxVelocity(Math.abs(getTurnRemaining()) > 33 ? 0 : MAX_VELOCITY);
+    }
+
+    static Point2D vectorToLocation(double angle, double length, Point2D sourceLocation) {
+        return vectorToLocation(angle, length, sourceLocation, new Point2D.Double());
+    }
+
+    static Point2D vectorToLocation(double angle, double length, Point2D sourceLocation, Point2D targetLocation) {
+        targetLocation.setLocation(sourceLocation.getX() + Math.sin(angle) * length,
+                sourceLocation.getY() + Math.cos(angle) * length);
+        return targetLocation;
+    }
+
+    static double absoluteBearing(Point2D source, Point2D target) {
+        return Math.atan2(target.getX() - source.getX(), target.getY() - source.getY());
     }
 
     /**
